92cf2786b2525309ef8f2390db4464e6aaecf16f
gabrielesarwar <43757103+gabrielesarwar@users.noreply.github.com>
2019-02-26 00:32:07 -0500

    Update part2.c

--- part2.c
+++ part2.c
@@ -22,6 +22,7 @@ void print_matrix (long (*matrix)[Size_of_matrix])
         }
         printf("\n");
     }
+		printf("\n");
 }
 
 /**
@@ -45,6 +46,108 @@ void row_multiplication (int firstrow, int desired_rows, long (*m)[Size_of_matri
                 out[i][z] += m[i][j] * n[j][z];
             }
         }
+    }
+}
+
+int main (int argc, char **argv)
+{
+		
+		//memory stuff
+    int shmid = shmget((key_t)1234, sizeof(long) * Size_of_matrix * Size_of_matrix, 0666 | IPC_CREAT); 
+    if (shmid == -1) {
+        fprintf(stderr, "Failed to get shared memory: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    finalmatrix = shmat(shmid, NULL, 0);
+    if ((int)finalmatrix == -1) {
+        fprintf(stderr, "Failed to attach shared memory: %s\n", strerror(errno));
+        exit(1);
+    }
+
+  //  test case matricees
+	long matrix_1[Size_of_matrix][Size_of_matrix] = {
+				{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{4, 3, 2, 1},
+        {8, 7, 6, 5}};
+
+	long matrix_2[Size_of_matrix][Size_of_matrix] = {
+       {1, 3, 5, 7},
+       {2, 4, 6, 8},
+       {7, 3, 5, 7},
+       {8, 6, 4, 2}};
+  //calculate time passed like we did in labs
+    struct timeval start_time;
+		struct timeval end_time;
+    gettimeofday(&start_time, NULL);
+			 
+			 //read argc for argument count
+    int child_processes = 1;
+    
+    if (argc > 1) {
+    char *end;
+    	long argument = strtol(argv[1], &end, 10);
+       //assuming its a valid value, 1 2 or 4
+       
+	printf("argument is %d\n", argc);
+	
+        child_processes = Size_of_matrix / argument;
+    }
+    
+  //default argument is 4
+//----------------------------------------------------------------------------------------
+    pid_t subprocesses[Size_of_matrix];
+    
+    for (int i = 0; i < Size_of_matrix; i += child_processes) {
+       
+     subprocesses[i] = fork();
+        
+    if (subprocesses[i] == -1) {
+            fprintf(stderr, "shmget failure\n");
+		        exit(EXIT_FAILURE);
+        } else if (subprocesses[i] == 0) {
+          
+            if (child_processes == 1) {
+                printf("row: %d\n", i);
+            } else {
+                printf(" rows, i is: %d\n ", i);
+                for (int z = 1; z < (child_processes - 1); z++) {
+                    printf(", %d", i + z);
+                }
+                printf("  %d\n", i + (child_processes - 1));
+            }
+            
+            row_multiplication(i, child_processes, matrix_1, matrix_2, finalmatrix);
+            exit(0);
+        }
+    }
+    
+    //iterate trhough child processes
+    for (int i = 0; i < Size_of_matrix; i++) {
+        waitpid(subprocesses[i], NULL, 0);
+    }
+   //---------------------------------------------------------------------------------------- 
+
+
+   
+    
+    gettimeofday(&end_time, NULL); //get the total time now since multiplication shud be done
+
+    printf("Time passed: %d in nano secs \n", time_interval(&start_time, &end_time));
+
+    printf("here is matrix 1, then 2, then the resulting multiplied matrix \n");
+    
+    print_matrix(matrix_1);
+    
+    print_matrix(matrix_2);
+ 
+    print_matrix(finalmatrix);
+    
+    shmdt(finalmatrix); //mmemory
+
+}
+
     }
 }
 
